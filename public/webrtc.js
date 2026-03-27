0b28f2b9dd80de0fc9c78f6316b2a2190ddbac71
Partha Sarathi <166997209+sarathi-dj@users.noreply.github.com>
2026-03-27 10:44:53 +0530

    Add metered TURN servers to ICE_SERVERS
    Added metered TURN servers for improved connectivity.

--- public/webrtc.js
+++ public/webrtc.js
@@ -4,7 +4,17 @@ const ICE_SERVERS = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' },
+    // ADD YOUR METERED TURN SERVERS BELOW:
+    {
+      urls: 'turn:global.relay.metered.ca:80',
+      username: '10c2bf8c532d2660267fee1c',
+      credential: 'metiiz5BkGRtAeuD'
+    },
+    {
+      urls: 'turns:global.relay.metered.ca:80:443?transport=tcp',
+      username: '10c2bf8c532d2660267fee1c',
+      credential: 'metiiz5BkGRtAeuD'
+    }
   ]
 };
 
